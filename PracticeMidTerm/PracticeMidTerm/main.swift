c3c24a130b3f70a6c5fd64ce9a4fba23f4d32351
Chae Haram <djvemfla2@gmail.com>
2022-04-19 00:06:43 +0900

    swift programming 중간고사

---
+++ PracticeMidTerm/PracticeMidTerm/main.swift
@@ -0,0 +1,60 @@
+//
+//  main.swift
+//  PracticeMidTerm
+//
+//  Created by Chae_Haram on 2022/04/18.
+//
+
+import Foundation
+
+// 1문제 : 정수 한 개를 입력받고 inch -> cm로 변환하라.
+
+//var inputInt = readLine()!
+//var transDouble = Double(inputInt)!
+//var transCm = transDouble * 2.54
+//print(transCm)
+
+// 2문제 : 정수 두 개 입력 받고 몫과 나머지를 리턴하는 함수를 구하여라.
+
+//var inputInt = readLine()!
+//var arr: [String] = []
+//arr = inputInt.components(separatedBy: " ")
+//var value1: Int = Int(arr[0])!
+//var value2: Int = Int(arr[1])!
+//
+//func devideCalc(num1: Int, num2: Int) -> Int {
+//    let resultDivide = num1 / num2
+//    return resultDivide
+//}
+//func remCalc(num1: Int, num2: Int) -> Int {
+//    let resultRem = num1 % num2
+//    return resultRem
+//}
+//
+//print(devideCalc(num1: value1, num2: value2))
+//print(remCalc(num1: value1, num2: value2))
+
+
+// 3문제 : 정수 한 개를 입력 받고 홀수인지 짝수인지 구하고 반복문으로 반복시켜라
+
+//while true {
+//    let inputInt = readLine()!
+//    let transInt = Int(inputInt)!
+//    var intValue: Int = 0
+//
+//    if transInt == 0 {
+//        intValue = 0
+//    } else if transInt % 2 == 1 {
+//        intValue = 1
+//    } else if transInt % 2 == 0 {
+//        intValue = 2
+//    } else {
+//        print("올바른 입력 아님.")
+//    }
+//
+//    switch intValue{
+//    case 1: print("입력한 정수는 홀수.")
+//    case 2: print("입력한 정수는 짝수.")
+//    default: print("입력한 정수는 홀수도 짝수도 아니다.")
+//    }
+//}
