b610d82d2dac68fc08cd3f8d11b0c10c8832fa80
Chae Haram <djvemfla2@gmail.com>
2022-06-09 01:25:18 +0900

    14주차 WorkSheet #11

--- WorkSheet10/WorkSheet10/main.swift
+++ WorkSheet10/WorkSheet10/main.swift
@@ -29,7 +29,7 @@ class Person {
         self.gender = .none
     }
     
-    func thinking() {
+    func thinking(height: Float, money: Int, car: Bool, name: String, weight: Float, color: String, live: String) {
     
     }
 }
@@ -60,8 +60,21 @@ class Man: Person {
         super.init(name: name, age: age, gender: .man)
     }
     
-    override func thinking() {
-        print("나의 이상형 \(idealWeight), \(idealColor), \(idealLive)")
+    override func thinking(height: Float, money: Int, car: Bool, name: String, weight: Float, color: String, live: String) {
+        var count = 0
+        if self.idealWeight >= weight {
+            count += 1
+        }
+        if self.idealLive == live {
+            count += 1
+        }
+        if self.idealColor == color {
+           count += 1
+        }
+        if count == 3 {
+            manIdealPick[self.name] = name
+        }
+        count = 0
     }
 }
 
@@ -85,8 +98,21 @@ class Woman: Person {
         super.init(name: name, age: age, gender: .woman)
     }
    
-    override func thinking() {
-        print("나의 이상형 \(idealHeight), \(idealMoney), \(idealCar)")
+    override func thinking(height: Float, money: Int, car: Bool, name: String, weight: Float, color: String, live: String) {
+        var count = 0
+        if self.idealHeight <= height {
+            count += 1
+        }
+        if self.idealMoney <= money {
+            count += 1
+        }
+        if self.idealCar == car {
+            count += 1
+        }
+        if count == 3 {
+            womanIdealPick[self.name] = name
+        }
+        count = 0
     }
 }
 
@@ -99,50 +125,34 @@ class Woman: Person {
  커플탄생 : (이순신, 유관순)
  */
 
-// 이상향의 부합하는 기준이 무엇?
-// 1개? or 과반수? or 전체? or 가장 가까운 사람?
-// 이상향의 프로퍼티가 각각 클래스에 있어야하는건지
-
-let 홍길동: Man = Man(name: "홍길동", age: 22, height: 173.3, money: 3000, car: false, idealWeight: 53.3, idealColor: "파랑", idealLive: "서울")
+let 홍길동: Man = Man(name: "홍길동", age: 22, height: 185.3, money: 2900, car: false, idealWeight: 55.0, idealColor: "파랑", idealLive: "서울")
 let 이순신: Man = Man(name: "이순신", age: 25, height: 175.5, money: 3300, car: false, idealWeight: 62.2, idealColor: "빨강", idealLive: "경기")
-let 을지문덕: Man = Man(name: "을지문덕", age: 28, height: 177.7, money: 3600, car: false, idealWeight: 55.5, idealColor: "검정", idealLive: "부산")
-let 세종대왕: Man = Man(name: "세종대왕", age: 31, height: 179.9, money: 3900, car: true, idealWeight: 57.7, idealColor: "하얀", idealLive: "인천")
-let 궁예: Man = Man(name: "궁예", age: 34, height: 182.2, money: 4200, car: true, idealWeight: 59.9, idealColor: "초록", idealLive: "대군")
+let 을지문덕: Man = Man(name: "을지문덕", age: 28, height: 177.7, money: 3600, car: true, idealWeight: 55.5, idealColor: "검정", idealLive: "부산")
+let 세종대왕: Man = Man(name: "세종대왕", age: 31, height: 182.2, money: 3900, car: true, idealWeight: 57.7, idealColor: "하얀", idealLive: "인천")
+let 궁예: Man = Man(name: "궁예", age: 34, height: 179.2, money: 4200, car: true, idealWeight: 59.9, idealColor: "초록", idealLive: "대구")
 
-let 갑순이: Woman = Woman(name: "갑순이", age: 20, weight: 53.3, color: "파랑", live: "서울", idealHeight: 175.5, idealMoney: 3300, idealCar: false)
-let 신사임당: Woman = Woman(name: "신사임당", age: 23, weight: 55.5, color: "검정", live: "부산", idealHeight: 175.5, idealMoney: 3300, idealCar: false)
-let 퀴리부인: Woman = Woman(name: "퀴리부인", age: 26, weight: 57.7, color: "하얀", live: "인천", idealHeight: 173.3, idealMoney: 3000, idealCar: false)
-let 장희빈: Woman = Woman(name: "장희빈", age: 29, weight: 59.9, color: "초록", live: "대구", idealHeight: 179.9, idealMoney: 3900, idealCar: true)
-let 유관순: Woman = Woman(name: "유관순", age: 32, weight: 62.2, color: "빨강", live: "경기", idealHeight: 175.5, idealMoney: 3300, idealCar: false)
+let 갑순이: Woman = Woman(name: "갑순이", age: 20, weight: 53.3, color: "파랑", live: "서울", idealHeight: 175.0, idealMoney: 3200, idealCar: false)
+let 신사임당: Woman = Woman(name: "신사임당", age: 23, weight: 55.5, color: "검정", live: "부산", idealHeight: 175.0, idealMoney: 3000, idealCar: false)
+let 퀴리부인: Woman = Woman(name: "퀴리부인", age: 26, weight: 54.7, color: "하얀", live: "인천", idealHeight: 183.0, idealMoney: 2800, idealCar: false)
+let 장희빈: Woman = Woman(name: "장희빈", age: 29, weight: 54.4, color: "초록", live: "대구", idealHeight: 180.0, idealMoney: 3800, idealCar: true)
+let 유관순: Woman = Woman(name: "유관순", age: 32, weight: 60.2, color: "빨강", live: "경기", idealHeight: 175.0, idealMoney: 3000, idealCar: false)
 
 let manList: [Man] = [홍길동, 이순신, 을지문덕, 세종대왕, 궁예]
 let womanList: [Woman] = [갑순이, 신사임당, 퀴리부인, 장희빈, 유관순]
-
-/*
- 남자 여자 리스트를 뽑아서
- 각각 남자별 이상형 점수표 배열 -> ex) [유관순: 2, 장희빈: 1, 퀴리부인: 3] -> 퀴리부인이 이상형
- 
- 남자에 대해 for문을 돌리고 {
-    여자에 대해 for문을 돌리고 {
-        if 여자의 조건 == 남자의 이상형 조건 {
-            count++
-        }
-    }
- }
- */
+var womanIdealPick: [String: String] = [:]
+var manIdealPick: [String: String] = [:]
+var couplePick: [String: String] = [:]
 
 for woman in womanList {
     for man in manList {
-        if woman.idealHeight == man.height && woman.idealMoney == man.money && woman.idealCar == man.car {
-            print("\(woman)의 이상형은 \(man)")
-        }
+        woman.thinking(height: man.height, money: man.money, car: man.car, name: woman.name, weight: woman.weight, color: woman.color, live: woman.live)
+        man.thinking(height: man.height, money: man.money, car: man.car, name: man.name, weight: woman.weight, color: woman.color, live: woman.live)
+        
     }
 }
 
-//for woman in 0..<womanList.count {
-//    for man in 0..<manList.count {
-//        if woman.idealCar == man.car {
-//            if var ideal = womanIdealCount[]
-//        }
-//    }
-//}
+
+
+print(manIdealPick)
+print(womanIdealPick)
+print(couplePick)
---
+++ WorkSheet11/WorkSheet11/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  WorkSheet11
+//
+//  Created by Chae_Haram on 2022/05/30.
+//
+
+import Foundation
+
+
+
